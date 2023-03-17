1f61b5ca4d23b0ff4db661997aad4b5ebb3cabd2
alishaiduc7 <alis.haiduc7773@gmail.com>
2023-03-17 23:39:58 +0200

    AH - Added database collections

---
+++ CareerSystemExpert/NewFolder2/MongoDBService.cs
@@ -0,0 +1,25 @@
+﻿using CareerSystemExpert.NewFolder1;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace CareerSystemExpert.NewFolder2
+{
+    public class MongoDBService
+    {
+
+        private readonly IMongoCollection<Career> _careersCollection;
+
+        public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings)
+        {
+            MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+            _careersCollection = database.GetCollection<Career>(mongoDBSettings.Value.CollectionName);
+        }
+
+      //  public async Task<List<Career>> GetAsync() { }
+        public async Task CreateAsync(Career career) { }
+        public async Task AddToPlaylistAsync(string id, string movieId) { }
+        public async Task DeleteAsync(string id) { }
+    }
+}
--- CareerSystemExpert/Program.cs
+++ CareerSystemExpert/Program.cs
@@ -1,4 +1,10 @@
+using CareerSystemExpert.NewFolder1;
+using CareerSystemExpert.NewFolder2;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+builder.Services.AddSingleton<MongoDBService>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -12,6 +18,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
